fcca8cfa85697e4cb12b1c6d5aeafd0598db7238
quebranunca <142365314+quebranunca@users.noreply.github.com>
2026-05-23 00:43:48 -0300

    ajustes

--- PlataformaFutevolei.Api/Program.cs
+++ PlataformaFutevolei.Api/Program.cs
@@ -97,11 +97,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"
-            )
-            .AllowAnyOrigin()
+            .WithOrigins(origensFrontend)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
